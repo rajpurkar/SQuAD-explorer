7edf9c95208ff672e1afd0316e9717afa74aa260
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-02-12 21:04:34 -0800

    Add nightmare webpage save

---
+++ scrape.js
@@ -0,0 +1,17 @@
+var Nightmare = require('nightmare')
+var fs = require('fs')
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+var n = new Nightmare({
+  switches: {
+    'ignore-certificate-errors': true
+  }
+})
+n.goto('https://worksheets.codalab.org/worksheets/0x62eefc3e64e04430a1a24785a9293fff/')
+.wait(2000)
+.evaluate(function () {
+  return document.body.innerHTML
+})
+.end()
+.then(function (result) {
+  fs.writeFileSync('./saved.html', result)
+})
