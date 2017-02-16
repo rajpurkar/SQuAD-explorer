f0abf6edb36c14f65e372b8390ff36ceabf4bc77
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-02-16 01:10:03 -0800

    Finish scraping processing

--- parse.js
+++ parse.js
@@ -1,6 +1,7 @@
 var cheerio = require('cheerio')
 var fs = require('fs')
 var $ = cheerio.load(fs.readFileSync('./saved.html'))
+var jsonfile = require('jsonfile')
 
 function getEntries (split) {
   var parent = $('h2#' + split + '-set-leaderboard').closest('.ws-item').next()
@@ -8,14 +9,35 @@ function getEntries (split) {
   $(parent).find('tbody > tr').each(function () {
     var entry = {}
     var cells = $(this).find('td')
-    entry.description = cells.eq(1).text()
+    entry.description = cells.eq(1).text().trim()
     entry.f1 = parseFloat(cells.eq(4).text())
     entry.em = parseFloat(cells.eq(3).text())
     entry.date = cells.eq(2).text().trim()
     entries.push(entry)
   })
+  entries.sort(function (a, b) {
+    var f1Diff = b.f1 - a.f1
+    if (f1Diff === 0) {
+      var emDiff = b.em - a.em
+      return emDiff
+    } else {
+      return f1Diff
+    }
+  })
+
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i]
+    if (i === 0) {
+      entry.rank = 1
+    } else {
+      var prevEntry = entries[i - 1]
+      var rank = prevEntry.rank
+      if (entry.em < prevEntry.em && entry.f1 < prevEntry.f1) rank++
+      entry.rank = rank
+    }
+  }
   return entries
 }
 
-getEntries('development')
-console.log(getEntries('test'))
+jsonfile.writeFileSync('./development.json', getEntries('development'))
+jsonfile.writeFileSync('./test.json', getEntries('test'))
--- scrape.js
+++ scrape.js
@@ -1,11 +1,13 @@
 var Nightmare = require('nightmare')
 var fs = require('fs')
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+
 var n = new Nightmare({
   switches: {
     'ignore-certificate-errors': true
   }
 })
+
 n.goto('https://worksheets.codalab.org/worksheets/0x62eefc3e64e04430a1a24785a9293fff/')
 .wait(2000)
 .evaluate(function () {
